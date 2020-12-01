ae94702b180780a3c28ec2c8c478d66d5c7be889
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2020-12-01 08:27:14 -0300

    feature: add import file

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,15 +10,17 @@ interface HeaderProps {
   size?: 'small' | 'large';
 }
 
-const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => (
-  <Container size={size}>
-    <header>
-      <img src={Logo} alt="GoFinances" />
-      <nav>
-        <Link to="/import">Importar</Link>
-      </nav>
-    </header>
-  </Container>
-);
+const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => {
+  return (
+    <Container size={size}>
+      <header>
+        <img src={Logo} alt="GoFinances" />
+        <nav>
+          <Link to="/import">Importar</Link>
+        </nav>
+      </header>
+    </Container>
+  );
+};
 
 export default Header;
